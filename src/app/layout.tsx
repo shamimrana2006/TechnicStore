c0d90c7be90f0bc5c1defd54f32f4de29c62ae13
shamimranapro85 <shamimranaprofessional@gmail.com>
2025-10-11 16:00:34 +0600

    footer add but in complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navigtation from "@/MasterLayout/Navigtation";
+import { FooterWithSocialLinks } from "./components/sections/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <Navigtation />
         {children}
+        <FooterWithSocialLinks />
       </body>
     </html>
   );
